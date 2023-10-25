a0ddeaf031b3a863df0e393364a7697dc97854af
Barbé Maël <mael.barbe47@gmail.com>
2023-10-25 02:00:31 +0200

    Ajout du carousel

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,45 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+const slide_left = document.querySelector(".arrow_left");
+const slide_right = document.querySelector(".arrow_right");
+const img = document.querySelector('.banner-img')
+const p = document.querySelector('#banner p')
+const dots = document.querySelectorAll('.dot');
+let i = 0;
+
+function updateDots() {
+
+	for (let dotIndex = 0; dotIndex < dots.length; dotIndex++) {
+		const dot = dots[dotIndex];
+		
+		if(dotIndex == i)
+		{
+			dot.classList.add('dot_selected');
+		}
+		else {
+			dot.classList.remove('dot_selected');
+}}}
+
+slide_left.addEventListener("click", () => {
+	if (i === 0){
+        i = slides.length -1;
+    } else{
+        i--;
+    }
+	updateDots();
+	img.src=`assets/images/slideshow/${slides[i].image}`
+	p.innerHTML = slides[i].tagLine;
+});
+
+slide_right.addEventListener("click", () => {
+	if (i === slides.length -1){
+        i = 0;
+    } else{
+        i++;
+    }
+	updateDots();
+	img.src=`assets/images/slideshow/${slides[i].image}`
+	p.innerHTML = slides[i].tagLine;
+});
